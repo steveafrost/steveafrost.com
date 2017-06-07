444da878350208489d77439cc514cd2f5fc01694
Steve Frost <hello@steveafrost.com>
2017-06-07 00:03:00 -0400

    updated to Noreiga typography package & changed bio to match site title

--- components/Bio.js
+++ components/Bio.js
@@ -11,11 +11,11 @@ class Bio extends React.Component {
           marginBottom: rhythm(2.5),
         }}
       >
-        I'm a <strong>software engineer</strong> who lives in Brooklyn and codes in Javascript, PHP, and Ruby.
+        I'm a <strong>web developer</strong> who lives in Brooklyn and codes in Javascript, Ruby, and PHP.
         <br /><br />
         When I'm not programming, I'm either biking, running, or at the community garden.
         <br /><br />
-        Find me on <a href="https://steveafrost.com" target="_blank">GitHub</a>, <a href="https://linkedin.com/in/steveafrost.com" target="_blank">LinkedIn</a>, or <a href="https://twitter.com/SteveEff">Twitter</a>!
+        Find me on <a href="https://steveafrost.com" target="_blank">GitHub</a>, <a href="https://linkedin.com/in/steveafrost.com" target="_blank">LinkedIn</a>, or <a href="https://twitter.com/SteveEff" target="_blank">Twitter</a>!
       </p>
     )
   }
--- html.js
+++ html.js
@@ -4,10 +4,6 @@ import { prefixLink } from 'gatsby-helpers'
 import { GoogleFont, TypographyStyle } from 'react-typography'
 import typography from './utils/typography'
 
-import generateMetaTags from './utils/generateMetaTags'
-import { config } from 'config';
-import last from 'lodash/last';
-
 const BUILD_TIME = new Date().getTime()
 
 module.exports = React.createClass({
@@ -18,9 +14,6 @@ module.exports = React.createClass({
   render () {
     const { body } = this.props
     const head = Helmet.rewind();
-    const post = this.props.routes ? last(this.props.routes).page : null;
-    const path = this.props.location ? this.props.location.pathname : null;
-    const metaTags = generateMetaTags(post, config, path);
 
     let css
     if (process.env.NODE_ENV === 'production') {
@@ -36,7 +29,6 @@ module.exports = React.createClass({
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          {metaTags}
           {head.title.toComponent()}
           {head.meta.toComponent()}
           <TypographyStyle typography={typography} />
