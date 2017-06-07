b672753d074b4bad0ae1535ab98e8e21d09fa5ce
Steve Frost <hello@steveafrost.com>
2017-06-06 21:57:13 -0400

    ported over more posts & pics. edited bio. updated readme.md

--- components/Bio.js
+++ components/Bio.js
@@ -11,18 +11,21 @@ class Bio extends React.Component {
           marginBottom: rhythm(2.5),
         }}
       >
+        I'm a <strong>software engineer</strong> who lives in Brooklyn and codes in Javascript, PHP, and Ruby.
+        <br /><br />
+        When I'm not programming, I'm either biking, running, at a concert, or at the community garden.
+        <br /><br />
+        Find me on <a href="https://steveafrost.com" target="_blank">GitHub</a>, <a href="https://linkedin.com/in/steveafrost.com" target="_blank">LinkedIn</a>, or <a href="https://twitter.com/SteveEff">Twitter</a>!
         <img
           src={profilePic}
           alt={`author ${config.authorName}`}
           style={{
-            float: 'left',
             marginRight: rhythm(1/4),
             marginBottom: 0,
             width: rhythm(2),
             height: rhythm(2),
           }}
         />
-        Written by <strong>{config.authorName}</strong> who lives and works in San Francisco building useful things. <a href="https://twitter.com/kylemathews">You should follow him on Twitter</a>
       </p>
     )
   }
