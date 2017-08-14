43beadace247e409b4c8c858932119a46f8dd62c
Steve Frost <hello@steveafrost.com>
2017-08-13 19:36:10 -0500

    add back in the default nav until new nav is complete

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,7 +9,48 @@ import customCSS from "../css/custom.css"
 class Template extends React.Component {
   render() {
     const { location, children } = this.props
-    let header = (<Nav />)
+    let header
+    if (location.pathname === "/") {
+      header = (
+        <h3
+          style={{
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: "none",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+            to={"/"}
+          >
+            Steve Frost | Full-Stack Web Developer
+          </Link>
+        </h3>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            marginTop: 0,
+            marginBottom: rhythm(-1),
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: "none",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+            to={"/"}
+          >
+            Steve Frost | Full-Stack Web Developer
+          </Link>
+        </h3>
+      )
+    }
 
     return (
       <Container
