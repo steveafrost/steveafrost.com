975ac7cadcace601cf85cdfba7ef3ffb3ad62b8a
Steve Frost <hello@steveafrost.com>
2017-11-17 21:13:18 -0600

    updated dependencies, css, and index.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,5 @@
 import Typography from 'typography'
-import CodePlugin from 'typography-plugin-code'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
-
-Wordpress2016.plugins = [
-  new CodePlugin(),
-]
 
 const typography = new Typography(Wordpress2016)
 
