dc59e300c9cbbcc0d577a1a6f4af7b447a40439c
Steve Frost <sfrost@nerdery.com>
2018-01-30 12:26:01 -0600

    setup custom Typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,29 @@
 import Typography from 'typography';
 import CodePlugin from 'typography-plugin-code';
-import twinPeaks from 'typography-theme-twin-peaks';
 
-twinPeaks.plugins = [new CodePlugin()];
+const typography = new Typography({
+  title: 'Steve Frost - Portfolio',
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  headerFontFamily: ['Raleway', 'san-serif'],
+  bodyFontFamily: ['Merriweather', 'serif'],
+  googleFonts: [
+    { name: 'Raleway', styles: ['400', '700', '900'] },
+    { name: 'Merriweather', styles: ['400', '400i', '700'] },
+  ],
+  overrideStyles: () => ({
+    body: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    a: {
+      color: '#3c6e71',
+    },
+  }),
+});
 
-const typography = new Typography(twinPeaks);
+typography.plugins = [new CodePlugin()];
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
