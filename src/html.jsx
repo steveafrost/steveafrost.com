73a79196bbb87b676df08e358516f6e20aff1231
Steve Frost <sfrost@nerdery.com>
2018-01-30 13:14:15 -0600

    simplify html template w/ deconstruction, jsx, and single getCSS function

--- src/html.jsx
+++ src/html.jsx
@@ -2,39 +2,37 @@ import React from 'react';
 import { TypographyStyle, GoogleFont } from 'react-typography';
 import typography from './utils/typography';
 
-let stylesStr;
-if (process.env.NODE_ENV === 'production') {
-  try {
-    stylesStr = require('!raw-loader!../public/styles.css');
-  } catch (e) {
-    console.log(e);
+const getCSS = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return (
+      <style
+        id="gatsby-inlined-css"
+        dangerouslySetInnerHTML={{ __html: require('!raw-loader!../public/styles.css') }}
+      />
+    );
   }
-}
+  return undefined;
+};
 
-let css;
-if (process.env.NODE_ENV === 'production') {
-  css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />;
-}
-
-export default props => (
+export default ({ body, headComponents, postBodyComponents }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {props.headComponents}
+      {headComponents}
+      {getCSS()}
       <TypographyStyle typography={typography} />
       <GoogleFont typography={typography} />
-      {css}
     </head>
     <body>
       <div
         id="___gatsby"
         dangerouslySetInnerHTML={{
-          __html: props.body,
+          __html: body,
         }}
       />
-      {props.postBodyComponents}
+      {postBodyComponents}
     </body>
   </html>
 );
