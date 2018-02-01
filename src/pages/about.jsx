db70dd5e0a6e68bb65248eef5d0fc23861ffa427
Steve Frost <sfrost@nerdery.com>
2018-01-31 19:37:51 -0600

    add content to About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,44 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h1>About - Coming Soon</h1>
+    <p>
+      I've been a technology enthusiast as far back as I can remember, whether it was building
+      custom computers for friends, helping a local business with a website, or getting caught
+      sneaking on the family computer late at night in my high school years.
+    </p>
+
+    <p>
+      It was natural then that I progressed to learning how to operate computers in my college
+      years. In college I spent spare hours learning HTML/CSS/Javascript and tinkering with the
+      first smart phones - think Windows Mobile 6.1 Pocket PC.
+    </p>
+
+    <p>
+      After a few years in the professional field, I realized I wanted to get back into programming.
+      In 2016, after a few months of self-teaching from sites around the internet, I took the plunge
+      into the Full-Stack Development program at Flatiron School. Each day at Flatiron led me on a
+      journey to learn smarter, express myself creatively, and form tight bonds with my fellow
+      students.
+    </p>
+    <p>
+      Nowadays I'm either polishing up on my Angular knowledge or learning a new framework - most
+      recently it's been Node & React. Currently, there is a refreshing mix of personal projects
+      that I'm passionate about and projects that help out businesses/individuals in the immediate
+      community. It is unexplicably rewarding to use the skills I've acquired at Flatiron and
+      afterward to make a positive impact in the world.
+    </p>
+    <p>
+      When I'm not coding, I am usually riding my bike - either on casual trips or in organized
+      tours. If it is Saturday or Sunday morning, chances are you'll find me volunteering somewhere
+      around town.
+    </p>
+    <p>Contact me directly at hello[at]steveafrost.com</p>
+    <p>
+      See my GitHub repositories at{' '}
+      <a href="https://github.com/steveafrost" target="_blank">
+        https://github.com/steveafrost
+      </a>
+    </p>
   </div>
 );
 
