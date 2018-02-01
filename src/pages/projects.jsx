50ab866c54f4bf4bde6e3748d97b3cb88021b1f3
Steve Frost <sfrost@nerdery.com>
2018-01-31 19:52:41 -0600

    setup list items for Projects list

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ProjectList = () => (
-  <div>
-    <h1>Project List - Coming Soon</h1>
-  </div>
+const ProjectList = styled.ul`
+  list-style: none;
+`;
+
+export default () => (
+  <ProjectList>
+    <li>
+      <h2>Blind Tails</h2>
+    </li>
+    <li>
+      <h2>PJ Masks Live</h2>
+    </li>
+    <li>
+      <h2>Daily Documentary</h2>
+    </li>
+    <li>
+      <h2>Plant Nanny</h2>
+    </li>
+    <li>
+      <h2>Much Free, Very Wow</h2>
+    </li>
+    <li>
+      <h2>MLB Scoreboard</h2>
+    </li>
+    <li>
+      <h2>Brooklyn Queen Honey</h2>
+    </li>
+    <li>
+      <h2>Pittman Home Remodeling</h2>
+    </li>
+  </ProjectList>
 );
-
-export default ProjectList;
